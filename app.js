0b4db90b9ada27cfa7308e2edbfacb68b42ad868
vividxz <aa5454@srmist.edu.in>
2024-05-24 19:55:34 +0530

    Episode-4 part 1

--- app.js
+++ app.js
@@ -2,27 +2,67 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 
-const FuctionalComponent = ()=>(
-    <div>   
-        <h1>Functional component 🚀</h1>
+
+const HeaderComponent = ()=>{
+    return (
+        <div className="header-container">
+            <div>   
+                <img alt="logo" className="food-logo" src="https://png.pngtree.com/png-clipart/20220628/original/pngtree-food-logo-png-image_8239850.png"></img>
+            </div>
+            <div className="nav-div">   
+                <ul className="nav-items">  
+                    <li> <h2>Home</h2></li>
+                    <li><h2>Login</h2></li>
+                    <li><h2>Orders</h2></li>
+                    <li><h2>Cart</h2></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = ()=>{
+    return (
+        <div className="res-card">
+            <img className="food-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/1/0ed62bec-fb43-4815-8462-b1ff1fca6f1c_698364.jpg"></img>
+            <div className="card-details">  
+                <h2 className="food-info">McDonald's</h2>
+                <h4 className="food-info">North Indian, Biryani</h4>
+                <h4 className="food-info">4.3 ⭐</h4>
+            </div>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = ()=>(
+    <div className="app-layout">
+        <HeaderComponent/>
+        <Body/>
+        {/* <Footer/> */}
     </div>
 )
-const Title = ()=>(
-    <div>   
-        <h1>Namaste Reacat 🚀</h1>
-    </div>
-)
-
-
-const HeadingComponent = () => (
-    <div>
-        {FuctionalComponent()}
-        <Title/>
-        <h1>
-            Namaster React from Component
-        </h1>
-    </div>)
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
