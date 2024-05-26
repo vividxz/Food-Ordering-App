a99188bfbdf901c34ad742121d870243a8f6d27f
vividxz <aa5454@srmist.edu.in>
2024-05-27 00:52:29 +0530

    Episode 5 (useEffect, Shimmer UI)

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,35 @@
 import RestaurantCard from "./RestaurantCard";
-import resCards from "../utils/mockData";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 
 
 
 
 const Body = () => {
-  let [listCard, setlistCard] = useState(resCards);
+  let [listCard, setlistCard] = useState([]);
+  useEffect(() => {fetchData()}, []);
+
+  
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const dataVal = await data.json();
+
+    const resLiveData = dataVal?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    setlistCard(resLiveData);
+
+  }
+
+  if(listCard.length === 0){
+     return <h1>Loading....</h1>
+  }
+
     return (
       <div className="body">
         <div >
           <button 
             className="filter-btn" 
             onClick={()=>{
-              setlistCard(resCards.filter((res)=>res.info.avgRating>4));
+              setlistCard(listCard.filter((res)=>res.info.avgRating>4));
             }}>Filter</button>
         </div>
         <div className="res-container">
---
+++ src/components/Shimmer.js
@@ -0,0 +1,18 @@
+const shimmer = () => {
+    return 
+        <div className="shimmer-container">   
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+        </div>
+}
+
+
+export default shimmer;
