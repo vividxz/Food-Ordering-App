399ccd7b95a21ceffcab2ea05939545e5e9b201c
vividxz <aa5454@srmist.edu.in>
2024-05-26 22:01:24 +0530

    Episode 4 Hooks useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
 import resCards from "../utils/mockData";
+import { useState } from "react";
+
+
+
+
+
 
 const Body = () => {
+  let [listCard, setlistCard] = useState(resCards);
     return (
       <div className="body">
-        <div className="search">Search</div>
+        <div >
+          <button 
+            className="filter-btn" 
+            onClick={()=>{
+              setlistCard(resCards.filter((res)=>res.info.avgRating>4));
+            }}>Filter</button>
+        </div>
         <div className="res-container">
         
-          {resCards.map((restaurant,index) => (
+          {listCard.map((restaurant,index) => (
             <RestaurantCard key={index} resData={restaurant} />
           ))}
         </div>
