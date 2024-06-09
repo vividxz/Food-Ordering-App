0490b58527baf4d89341bdcd2f44e2769607135a
vividxz <aa5454@srmist.edu.in>
2024-06-09 14:42:15 +0530

    new Cors Added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const CDN_IMG =  "https://media-assets.swiggy.com/swiggy/image/upload/fl_
 
 export const LOGO_IMG = "https://cdn2.iconfinder.com/data/icons/food-restaurant-1/128/flat-51-512.png";
 
-export const MENU = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.89960&lng=80.22090&restaurantId=";
+export const MENU = "https://corsproxy.io/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.89960&lng=80.22090&restaurantId=";
